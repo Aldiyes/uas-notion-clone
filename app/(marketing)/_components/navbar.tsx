995b44a372b25a4afc151a6a1dbd92e45d65c76d
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2024-06-03 05:13:39 +0700

    feat: theme provider

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from "@/lib/utils";
 
+import { ModeToggle } from "@/components/mode-toggle";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { Logo } from "./logo";
 
@@ -11,13 +12,13 @@ export const Navbar = () => {
   return (
     <div
       className={cn(
-        "fixed top-0 z-50 flex w-full items-center bg-background p-6",
+        "fixed top-0 z-50 flex w-full items-center bg-background p-6 dark:bg-[#1F1F1F]",
         scrolled && "border-b shadow-sm",
       )}
     >
       <Logo />
       <div className="flex w-full items-center justify-between gap-x-2 md:ml-auto md:justify-end">
-        {/* TODO: Toggle Theme */}
+        <ModeToggle />
       </div>
     </div>
   );
