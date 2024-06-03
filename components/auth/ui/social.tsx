2c3f2d8a844ae3e82c7c3289f7951ecacf4d7c55
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2024-06-03 15:18:48 +0700

    feat(auth): add OAuth

--- components/auth/ui/social.tsx
+++ components/auth/ui/social.tsx
@@ -3,10 +3,17 @@
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { signIn } from "next-auth/react";
+
 import { Button } from "@/components/ui/button";
 
 export const Social = () => {
-  const onClick = (provider: "google" | "github") => {};
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
   return (
     <div className="flex w-full items-center gap-x-2">
       <Button
